4fbc4a616317f49af8753a9870e5c8a5a36c9805
jeongwon <jeongwon201@naver.com>
2022-10-17 17:14:26 +0900

    first push

---
+++ src/main/java/com/kafka/KafkaController.java
@@ -0,0 +1,23 @@
+package com.kafka;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private KafkaProducer producer;
+
+    private KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping
+    public String sendMessage(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+
+        return "success";
+    }
+}
